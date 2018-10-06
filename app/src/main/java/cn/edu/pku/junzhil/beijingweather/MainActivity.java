eb0ed6cf6698c932d20de6f634b8d8fbd57bf91e
junzhiL <843334966@qq.com>
2018-10-06 20:18:47 +0800

    init project

---
+++ app/src/main/java/cn/edu/pku/junzhil/beijingweather/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.pku.junzhil.beijingweather;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_layout);
+
+    }
+}
