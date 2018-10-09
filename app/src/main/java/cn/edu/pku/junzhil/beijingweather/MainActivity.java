f640e7111f391b0657b09fdd6b928d6a81f7bfe8
junzhiL <843334966@qq.com>
2018-10-09 19:47:24 +0800

    problem solved

--- app/src/main/java/cn/edu/pku/junzhil/beijingweather/MainActivity.java
+++ app/src/main/java/cn/edu/pku/junzhil/beijingweather/MainActivity.java
@@ -3,7 +3,10 @@ package cn.edu.pku.junzhil.beijingweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import cn.edu.pku.junzhil.util.NetUtil;
 
 
 public class MainActivity extends Activity {
@@ -11,6 +14,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_layout);
-
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+            Log.d("myweather","网络OK");
+            Toast.makeText(MainActivity.this,"网络OK！",Toast.LENGTH_LONG).show();
+        }else {
+            Log.d("myweather","网络挂了");
+            Toast.makeText(MainActivity.this,"网络挂了！",Toast.LENGTH_LONG).show();
+        }
     }
 }
