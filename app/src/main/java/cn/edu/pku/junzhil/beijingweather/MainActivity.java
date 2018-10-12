c24815b9aa03618ec3d22d733c5589edda5efac3
junzhiL <843334966@qq.com>
2018-10-13 00:30:09 +0800

    图标已全部添加

--- app/src/main/java/cn/edu/pku/junzhil/beijingweather/MainActivity.java
+++ app/src/main/java/cn/edu/pku/junzhil/beijingweather/MainActivity.java
@@ -103,6 +103,87 @@ public class MainActivity extends Activity implements View.OnClickListener {
         temperatureTv.setText(todayWeather.getHigh()+"～"+todayWeather.getLow());
         climateTv.setText(todayWeather.getType());
         windTv.setText("风力"+todayWeather.getFengli());
+        if(todayWeather.getPm25()!=null){
+            int pm25 = Integer.parseInt(todayWeather.getPm25());
+            if(pm25<=50){
+                pmImg.setImageResource(R.drawable.biz_plugin_weather_0_50);
+            }else if (pm25>=51 && pm25<=100){
+                pmImg.setImageResource(R.drawable.biz_plugin_weather_51_100);
+            }else if (pm25>=101 && pm25<=150){
+                pmImg.setImageResource(R.drawable.biz_plugin_weather_101_150);
+            }else if (pm25>=151 && pm25<=200){
+                pmImg.setImageResource(R.drawable.biz_plugin_weather_151_200);
+            }else if (pm25>=201 && pm25<=300){
+                pmImg.setImageResource(R.drawable.biz_plugin_weather_201_300);
+            }else if (pm25>300){
+                pmImg.setImageResource(R.drawable.biz_plugin_weather_greater_300);
+            }
+        }
+
+        switch (todayWeather.getType()){
+            case "晴":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_qing);
+                break;
+            case "阴":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_yin);
+                break;
+            case "雾":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_wu);
+                break;
+            case "多云":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_duoyun);
+                break;
+            case "小雨":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_xiaoyu);
+                break;
+            case "中雨":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_zhongyu);
+                break;
+            case "大雨":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_dayu);
+                break;
+            case "阵雨":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_zhenyu);
+                break;
+            case "雷阵雨":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_leizhenyu);
+                break;
+            case "雷阵雨加暴":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_leizhenyubingbao);
+                break;
+            case "暴雨":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_baoyu);
+                break;
+            case "大暴雨":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_dabaoyu);
+                break;
+            case "特大暴雨":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_tedabaoyu);
+                break;
+            case "阵雪":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_zhenxue);
+                break;
+            case "暴雪":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_baoxue);
+                break;
+            case "大雪":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_daxue);
+                break;
+            case "小雪":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_xiaoxue);
+                break;
+            case "雨夹雪":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_yujiaxue);
+                break;
+            case "中雪":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_zhongxue);
+                break;
+            case "沙尘暴":
+                weatherImg.setImageResource(R.drawable.biz_plugin_weather_shachenbao);
+                break;
+            default:
+                break;
+        }
         Toast.makeText(MainActivity.this,"更新成功",Toast.LENGTH_SHORT).show();
     }
 
